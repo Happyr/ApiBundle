ffb0a3499e8bd6da4c2d32c480b6d4dbc1c735de
Nyholm <tobias.nyholm@gmail.com>
2019-12-26 20:19:35 +0100

    Remove legacy code

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,12 +16,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('happyr_api');
-        // Keep compatibility with symfony/config < 4.2
-        if (!method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->root('happyr_api');
-        } else {
-            $rootNode = $treeBuilder->getRootNode();
-        }
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
